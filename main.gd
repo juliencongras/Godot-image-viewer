671928ee71efc9c2b8ae63876e19390610f559d0
juliencongras <julien.congras@gmail.com>
2024-02-23 15:08:55 +0100

    Can't figure out how to change thumbnail size

--- main.gd
+++ main.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var file_dialog = $FileDialog
+@onready var scroll_container = $ScrollContainer
+@onready var thumbnails_container = $"ScrollContainer/Thumbnails container"
+var filesInFolder = []
+var thumbnailSize = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,11 +13,37 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	thumbnails_container.columns = scroll_container.get_rect().size.x / thumbnailSize
 
 func _on_open_folder_button_pressed():
 	file_dialog.visible = true
 
 func _on_file_dialog_dir_selected(dir):
-	pass # Replace with function body.
+	for gridImage in thumbnails_container.get_children():
+		gridImage.queue_free()
+	var folderPath = DirAccess.open(dir)
+	folderPath.list_dir_begin()
+	var file_name = folderPath.get_next()
+	while file_name != "":
+		if file_name.get_extension() == "png" or file_name.get_extension() == "jpg" :
+			var fullPath = str(dir, "/", file_name)
+			filesInFolder.append(fullPath)
+		file_name = folderPath.get_next()
+	for file in filesInFolder:
+		var newImage = TextureRect.new()
+		var imageFile = Image.load_from_file(file)
+		var thumbnailImageOriginalSize = imageFile.get_size()
+		if thumbnailImageOriginalSize.x > thumbnailImageOriginalSize.y:
+			var thumbnailHeight = thumbnailSize * thumbnailImageOriginalSize.y / thumbnailImageOriginalSize.x
+			imageFile.resize(thumbnailSize, thumbnailHeight)
+		else:
+			var thumbnailWidth = thumbnailSize * thumbnailImageOriginalSize.x / thumbnailImageOriginalSize.y
+			imageFile.resize(thumbnailWidth, thumbnailSize)
+		var textureFromImage = ImageTexture.create_from_image(imageFile)
+		newImage.texture = textureFromImage
+		thumbnails_container.add_child(newImage)
+		newImage.focus_mode = Control.FOCUS_CLICK
+		newImage.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+		#if newImage.focus_entered:
+		#	imageClicked.emit(file)
+		print(newImage.texture.resource_name)
