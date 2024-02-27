b91d6e294cacf36fe49bf37bbc50cd03a31ab340
juliencongras <julien.congras@gmail.com>
2024-02-27 16:04:25 +0100

    Added ability to zoom/dezoom on pictures

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
  An exercise to see if I can make an image viewer.
 
 To do:
-- Control image scaling
 - Move images
 - Add a border to thumbnails
--- image_display.gd
+++ image_display.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var imageDisplayed = $Image
 @onready var imageDimensions = $"Image dimensions"
+@onready var zoomSlider = $ZoomSlider
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,8 +31,15 @@ func _ready():
 			var newScale = imageHeight / imageDisplayed.texture.get_height()
 			imageDisplayed.scale *= Vector2(newScale, newScale)
 	#Center image
-	imageDisplayed.position.x += windowSize.x / 2 - (imageDisplayed.texture.get_width() * imageDisplayed.scale.x) / 2
-	imageDisplayed.position.y += windowSize.y / 2 - (imageDisplayed.texture.get_height() * imageDisplayed.scale.y) / 2
+	imageDisplayed.position.x = windowSize.x / 2 - (imageDisplayed.texture.get_width() * imageDisplayed.scale.x) / 2
+	imageDisplayed.position.y = windowSize.y / 2 - (imageDisplayed.texture.get_height() * imageDisplayed.scale.y) / 2
+	zoomSlider.value = imageDisplayed.scale.x
 
 func _on_close_image_pressed():
 	queue_free()
+
+func _on_zoom_slider_value_changed(value):
+	var windowSize = get_viewport_rect().size
+	imageDisplayed.scale = Vector2(value, value)
+	imageDisplayed.position.x = windowSize.x / 2 - (imageDisplayed.texture.get_width() * imageDisplayed.scale.x) / 2
+	imageDisplayed.position.y = windowSize.y / 2 - (imageDisplayed.texture.get_height() * imageDisplayed.scale.y) / 2
